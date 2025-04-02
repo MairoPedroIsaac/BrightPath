f84855a2933caf485e9a582b1fe8e7295ef67cba
MairoPedroIsaac <mairopedroisaac@gmail.com>
2025-04-02 23:33:38 +0200

    Updated project

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,10 +6,17 @@ dotenv.config();
 // Initialize Sequelize
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 5432, // Default PostgreSQL port
     dialect: "postgres",
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false // Use this cautiously; recommended for development only
+        }
+    },
     logging: false, // Disable logging (optional)
 });
+
 
 // Test the connection
 sequelize
